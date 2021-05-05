90b60e77150d988db599c7ad7e05f712310092cb
Duaatarazi <>
2021-05-05 13:32:03 +0000

    edit the html file

--- lab04.js
+++ lab04.js
@@ -1,17 +1,17 @@
 alert('welcome to home styling')
 
-var usercolor= prompt('slect the color of background:blue,green,yellow')
+var usercolor= prompt('slect the color of background:blue,yellow')
 
 if (usercolor==='blue')
      
      {
-      document.write( '<img src="https://www.renonation.sg/wp-content/uploads/The-Scientist-Telok-Blangah-HDB-Pink-Design-Theme-3.jpg" alt="living room4">')
+      document.write( '<img src="https://jane-athome.com/wp-content/uploads/2019/03/ACS_0504.jpg">')
        }
   
    else if(usercolor==="yellow")
   
    {
-     document.write(' <img src="https://www.renonation.sg/wp-content/uploads/The-Scientist-Telok-Blangah-HDB-Pink-Design-Theme-3.jpg" alt="living room4">'
+     document.write(' <img src="https://i.pinimg.com/564x/c9/86/7d/c9867d53266ca52ce8b48db574e22f9f.jpg" alt="living room4">'
      )
      }
    
